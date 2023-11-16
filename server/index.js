4fa142fda93a0da08c778d67a26d0a6b5a59499a
KneerazPandey <nirajpandeyniraj77@gmail.com>
2023-11-16 16:18:30 +0545

    added express server and socket io

--- server/index.js
+++ server/index.js
@@ -1,16 +1,30 @@
-import express from 'express';
-import mongoose from 'mongoose';
-
+import mongoose from "mongoose";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
+//* Middleware
+app.use(express.json());
 
-app.listen(PORT, async () => {
-    console.log(`Server is listning at port: ${PORT}`);
+const server = http.createServer(app);
+const io = new Server(server);
 
-        await mongoose.connect('mongodb://0.0.0.0:27017/tic_tac_toe');
-        console.log('Successfully connected to mongooseDB');
-    
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(PORT, async () => {
+  console.log(`Server is listning at port: ${PORT}`);
+  try {
+    await mongoose.connect("mongodb://0.0.0.0:27017/tic_tac_toe");
+    console.log("Successfully connected to mongooseDB");
+  } catch (error) {
+    console.log("Unable to connect to mongoose DB", error);
+  }
 });
