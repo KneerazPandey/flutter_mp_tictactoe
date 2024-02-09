2f836b4983c64e293040b5ba8f9dd3dd9ab1005e
KneerazPandey <nirajpandeyniraj77@gmail.com>
2024-02-09 21:37:02 +0545

    added the .evn file for flutter application

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@ import 'package:flutter_mp_tictactoe/provider/room_data_provider.dart';
 import 'package:flutter_mp_tictactoe/screens/game_screen.dart';
 import 'package:flutter_mp_tictactoe/screens/screens.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
--- lib/resources/socket_client.dart
+++ lib/resources/socket_client.dart
@@ -1,4 +1,5 @@
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class SocketClient {
   IO.Socket? socket;
@@ -6,7 +7,8 @@ class SocketClient {
   static SocketClient? _instance;
 
   SocketClient._internal() {
-    socket = IO.io('http://192.168.1.69:3000', <String, dynamic>{
+    String baseUrl = dotenv.env['BASE_URL'] ?? 'http://0.0.0.0:3000';
+    socket = IO.io(baseUrl, <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
     });
